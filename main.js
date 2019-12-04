2398e78d1e95f1f6c042038675320a053f421d21
Basile Paradis <basileparadis@HAL-9000.local>
2019-12-04 17:01:53 -0500

    Implémentation de JQuery pour pouvoir gérer l'affichage des travaux dans le carousel de la section 3

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 function doWithThisElement(element) {
 
-    //document.getElementById("fenetreProjets").style.display = "none";
+    console.log($("#imagesTravaux > .item").length);
 
-    if(document.getElementById("fenetreProjets").style.display === "none")
+    switch($("#imagesTravaux > .item").length)
     {
-        window.alert(document.getElementById("fenetreProjets").style.visibility)
-        document.getElementById("fenetreProjets").style.display === "block";
+        case 1:
+            $('#imagesTravaux').append("<div class='item' id='cadre2'><img alt='DeuxiemeImage' id='image2'></img></div>");
+            $('#imagesTravaux').append("<div class='item' id='cadre3'><img alt='TroisiemeImage' id='image3'></img></div>");
+            $('#imagesTravaux').append("<div class='item' id='cadre4'><img alt='QuatriemeImage' id='image4'></img></div>");
+            break;
+        case 2:
+            $('#imagesTravaux').append("<div class='item' id='cadre3'><img alt='TroisiemeImage' id='image3'></img></div>");
+            $('#imagesTravaux').append("<div class='item' id='cadre4'><img alt='QuatriemeImage' id='image4'></img></div>");
+            break;
+        case 3:
+            $('#imagesTravaux').append("<div class='item' id='cadre4'><img alt='QuatriemeImage' id='image4'></img></div>");
+            break;
+        case 4:
+            break;
+        default:
+            break;
     }
 
     switch(element.id)
     {
         // PhilippeCouette
         case '1':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/4.jpg' alt='TroisiemeImage' id='image3'></img>";
@@ -22,7 +41,12 @@ function doWithThisElement(element) {
             break;
         // Elvis
         case '2':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/4.jpg' alt='TroisiemeImage' id='image3'></img>";
@@ -33,17 +57,26 @@ function doWithThisElement(element) {
             break;
         // Fender
         case '3':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/4.jpg' alt='TroisiemeImage' id='image3'></img>";
             document.getElementById('image4').outerHTML = "<img src='./img/travaux/"+element.alt+"/5.jpg' alt='QuatriemeImage' id='image4'></img>";  
             document.getElementById('texte').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";            
-            console.log(document.getElementById('image1').outerHTML);
             break;
         // StenslandGirard
         case '4':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/1.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='TroisiemeImage' id='image3'></img>";
@@ -53,90 +86,135 @@ function doWithThisElement(element) {
             break;
         // LaBeteASeguin
         case '5':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').hide();
+            $('#myCarousel').find('#indicateur4').hide();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/1.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='DeuxiemeImage' id='image2'></img>";
-            document.getElementById('image3').outerHTML = "<img src='' alt='TroisiemeImage' id='image3'></img>";
-            document.getElementById('image4').outerHTML = "<img src='' alt='QuatriemeImage' id='image4'></img>";
             document.getElementById('texte').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-            console.log(document.getElementById('image1').outerHTML);
             break;
         // Parrafine
         case '6':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/1.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='TroisiemeImage' id='image3'></img>";
             document.getElementById('image4').outerHTML = "<img src='./img/travaux/"+element.alt+"/4.jpg' alt='QuatriemeImage' id='image4'></img>";
             document.getElementById('texte').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-            console.log(document.getElementById('image1').outerHTML);
             break;
         // PurPak
         case '7':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
+            $('#myCarousel').find('#indicateur3').show();
+            $('#myCarousel').find('#indicateur4').show();
             document.getElementById('image1').outerHTML = "<img src='./img/travaux/"+element.alt+"/1.jpg' alt='PremiereImage' id='image1'></img>";
             document.getElementById('image2').outerHTML = "<img src='./img/travaux/"+element.alt+"/2.jpg' alt='DeuxiemeImage' id='image2'></img>";
             document.getElementById('image3').outerHTML = "<img src='./img/travaux/"+element.alt+"/3.jpg' alt='TroisiemeImage' id='image3'></img>";
             document.getElementById('image4').outerHTML = "<img src='./img/travaux/"+element.alt+"/4.jpg' alt='QuatriemeImage' id='image4'></img>";
             document.getElementById('texte').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-            console.log(document.getElementById('image1').outerHTML);
             break;
         // ByeBye
         case '8':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/329202293' id='image1' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ production : Guillaume Lespérance et KOTV ++ réalisation : Simon-Olivier Fecteau (mettre le logo des Gémeaux finaliste 2019)";
-            console.log(document.getElementById('image1').outerHTML);
             break;
         // Cerebrum
         case '9':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/355572258' id='image1' width='640' height='360' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ Un résumé de quelques projets… ++ A resume of some of the work we did…";
-            console.log(document.getElementById('image4').outerHTML);
             break;
         // Demo
         case '10':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/253806432' id='image1' width='640' height='360' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "production : Josée Vallée / Datsit Sphère réalisation : Richard Blaimert";
-            console.log(document.getElementById('image4').outerHTML);
             break;
         // Gemeaux
         case '11':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/329202814' id='image1' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ production : Radio-Canada ++ réalisation : Guy Gagnon";
-            console.log(document.getElementById('image4').outerHTML);
             break;
         // LMalediction
         case '12':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/329202750' id='image1' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ À venir.";
             break;
         // Marika
         case '13':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/329203097' id='image1' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ production : Téléfiction";
             break;
         // NitroRush
         case '14':
-            document.getElementById('indicateurs').style.display = "block";
+            $('#myCarousel').find('#indicateurs').show();
+            $('#myCarousel').find('.carousel-control').show();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
+            $('#myCarousel').find('#indicateur1').show();
+            $('#myCarousel').find('#indicateur2').show();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/217019210' id='image1' width='640' height='270' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('image2').outerHTML="<iframe src='https://player.vimeo.com/video/217021460' id='image2' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ Un résumé de quelques projets… ++ A resume of some of the work we did…";
             break;
         // PauseKahwa
         case '15':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/234550377' id='image1' width='640' height='360' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ À venir.";
             break;
         // QcCinema
         case '16':
-            document.getElementById('indicateurs').style.display = "none";
+            $('#myCarousel').find('#indicateurs').hide();
+            $('#myCarousel').find('.carousel-control').hide();
+            $('#myCarousel').find('#cadre2').remove();
+            $('#myCarousel').find('#cadre3').remove();
+            $('#myCarousel').find('#cadre4').remove();
             document.getElementById('image1').outerHTML="<iframe src='https://player.vimeo.com/video/234550418' id='image1' width='640' height='360' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe>";
             document.getElementById('texte').innerHTML = "++ Réalisation : Guy Gagnon";
             break;
@@ -148,6 +226,13 @@ function doWithThisElement(element) {
 function upPage(document) {
 
     console.info(document.getAttribute("href"));
+
+    const numSection = document.getAttribute("href");
+
+    if(numSection<5)
+    {
+        numSection++;
+    }
 
     switch(document.getAttribute("href"))
     {
